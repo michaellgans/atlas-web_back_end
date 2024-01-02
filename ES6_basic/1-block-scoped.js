fee2fd161806f521f2a81a3ff9a2487b7c3856b1
michaellgans <michaellgans@Victoria>
2024-01-02 16:54:47 -0600

    Adjusting variables for hoisting

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    var newTask = true;
+    var newTask2 = false;
   }
 
   return [task, task2];
