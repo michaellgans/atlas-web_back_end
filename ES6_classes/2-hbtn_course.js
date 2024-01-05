bb13d06f98325a308e2fe028827d073e07d00559
michaellgans <michaellgans@Victoria>
2024-01-05 12:04:15 -0600

    Fixes linter

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -4,13 +4,13 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     /* Guard for types */
     if (typeof name !== 'string') {
-        throw TypeError('Name must be a string');
+      throw TypeError('Name must be a string');
     }
     if (typeof length !== 'number') {
-        throw TypeError('Length must be a number');
+      throw TypeError('Length must be a number');
     }
     if (!Array.isArray(students)) {
-        throw TypeError('Students must be an array of strings');
+      throw TypeError('Students must be an array of strings');
     }
     this._name = name;
     this._length = length;
