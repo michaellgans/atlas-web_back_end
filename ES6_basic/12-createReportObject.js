257e950f2cfda527c54fc6194a7550db50bac1fb
michaellgans <michaellgans@Victoria>
2024-01-05 09:53:11 -0600

    Trying to fix lint again

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,11 +1,8 @@
-/* import previous file */
-import createEmployeesObject from './11-createEmployeesObject.js';
-
 export default function createReportObject(employeesList) {
   /* Create empty object to return */
-  const employeeReport = {};
+  let employeeReport = {};
 
-  /* Store all VALUES from employeesList (like variadic)*/
+  /* Store all VALUES from employeesList (like variadic) */
   employeeReport = Object.assign({}, ...Object.values(employeesList));
 
   employeeReport.getNumberOfDepartments = function () {
