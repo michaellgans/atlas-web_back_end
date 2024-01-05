33d809337274c9e6275aae3f6e682ad58e88155d
michaellgans <michaellgans@Victoria>
2024-01-05 13:48:08 -0600

    Corrects format of output

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -35,7 +35,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this.amount} (${this.currency.displayFullCurrency})`;
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
