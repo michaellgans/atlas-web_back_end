5ff7daef6c65bcce2c0a3edea0b460005f90c156
michaellgans <michaellgans@Victoria>
2024-01-05 12:22:29 -0600

    Creates first part of task 3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+/* Task 3 */
+
+export default class Currency {
+  constructor (code, name) {
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(x) {
+    if (typeof x === 'string') {
+      this._code = x;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(x) {
+    if (typeof x === 'string') {
+      this._name = x;
+    }
+  }
+}
