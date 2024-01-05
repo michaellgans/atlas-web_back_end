d8ce2d5d418f21b274d76fa3f075b7b16b551b8e
michaellgans <michaellgans@Victoria>
2024-01-05 14:07:23 -0600

    Added override warning

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,8 +2,16 @@
 
 export default class Building {
   constructor(sqft) {
+    if (!this.evacuationWarningMessage) {
+        throw Error('Class extending Building must override evacuationWarningMessage')
+    }
     if (typeof sqft !== 'number') {
         throw TypeError('Sqft must be a number');
-    };
-  };
-};
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
