2b13334ee0c4a2696bfaec304eb1870a0d70c0bf
michaellgans <michaellgans@Victoria>
2024-01-05 15:12:54 -0600

    Edits toString output

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -3,10 +3,10 @@
 export default class Airport {
   constructor(name, code) {
     if (typeof name !== 'string') {
-        throw TypeError('Name must be a string');
+      throw TypeError('Name must be a string');
     }
     if (typeof code !== 'string') {
-        throw TypeError('Code must be a string');
+      throw TypeError('Code must be a string');
     }
     this._name = name;
     this._code = code;
