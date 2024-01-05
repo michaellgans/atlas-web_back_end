d9131549dc29eff8b135b95c1f9241d6fde2fb9a
michaellgans <michaellgans@Victoria>
2024-01-05 10:05:18 -0600

    Changes logic so object is on single line

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,13 +1,13 @@
 export default function createReportObject(employeesList) {
-  /* Create empty object to return */
-  let employeeReport = {};
-
-  /* Store all VALUES from employeesList (like variadic) */
-  employeeReport = Object.assign({}, ...Object.values(employeesList));
-
-  employeeReport.getNumberOfDepartments = function () {
-      /* returns number of departments */
+  /* Create object to return */
+  let employeeReport = {
+    allEmployees: {
+      ...employeesList,
+    },
+    /* Second thing to be stored in the object */
+    getNumberOfDepartments() {
       return Object.keys(employeesList).length;
+    },
   };
 
   return employeeReport;
