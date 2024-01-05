241799fd9b2acafc2e60c4fd4be577eeffcb500d
michaellgans <michaellgans@Victoria>
2024-01-05 16:29:55 -0600

    Getting rid of Type Errors

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -2,15 +2,6 @@
 
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw TypeError('Brand must be a string');
-    }
-    if (typeof motor !== 'string') {
-      throw TypeError('Motor must be a string');
-    }
-    if (typeof color !== 'string') {
-      throw TypeError('Color must be a string');
-    }
     this._brand = brand;
     this._motor = motor;
     this._color = color;
