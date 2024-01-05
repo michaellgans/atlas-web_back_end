029066c0094bc565eb686f85d3e6a8a2863b8b6b
michaellgans <michaellgans@Victoria>
2024-01-05 16:07:48 -0600

    Creates task 10

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,23 @@
+/* Task 10 */
+
+export default class Car {
+  constructor(brand, motor, color) {
+    if (typeof brand !== 'string') {
+      throw TypeError('Brand must be a string');
+    }
+    if (typeof motor !== 'string') {
+      throw TypeError('Motor must be a string');
+    }
+    if (typeof color !== 'string') {
+      throw TypeError('Color must be a string');
+    }
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const newCar = new Car(this._brand, this._motor, this._color);
+    return newCar;
+  }
+}
