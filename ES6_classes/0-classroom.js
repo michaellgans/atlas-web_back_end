ade7c4b6c1a11903edbdf4424af9070e7e91dde7
michaellgans <michaellgans@Victoria>
2024-01-05 10:26:42 -0600

    Fixes indentation for linter

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,7 +1,7 @@
 /* Task 0 */
 
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
