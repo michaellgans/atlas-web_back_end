2954abbdb513616d86085741daafcb17755013f6
michaellgans <michaellgans@Victoria>
2024-01-05 12:01:54 -0600

    Type check in constructor

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -2,6 +2,16 @@
 
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    /* Guard for types */
+    if (typeof name !== 'string') {
+        throw TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+        throw TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+        throw TypeError('Students must be an array of strings');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
