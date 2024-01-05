1470116a9d25e293a58e97954b2565347f33854d
michaellgans <michaellgans@Victoria>
2024-01-05 11:59:44 -0600

    reverting

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -2,57 +2,44 @@
 
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    /* Guard for types */
-    if (typeof name !== 'string') {
-        throw TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-        throw TypeError('Length must be a number');
-    }
-    if (!Array.isArray(students)) {
-        throw TypeError('Students must be an array of strings');
-    }
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  /* Getter and Setter for name */
   get name() {
     return this._name;
   }
 
   set name(x) {
-    if (typeof x !== 'string') {
-      throw TypeError('Name must be a string');
+    if (typeof x === 'string') {
+      this._name = x;
     } else {
-      return this._name;
+      return;
     }
   }
 
-  /* Getter and setter for length */
   get length() {
     return this._length;
   }
 
   set length(x) {
     if (typeof x === 'number') {
-      throw TypeError('Length must be a number');
+      this._length = x;
     } else {
-      return this._length;
+      return;
     }
   }
 
-  /* Getter and Setter for students */
   get students() {
     return this._students;
   }
 
   set students(x) {
-    if (!Array.isArray(x)) {
-      throw TypeError('Students must be an array of strings');
+    if (Array.isArray(x)) {
+      this._students = x;
     } else {
-      return this._students;
+      return;
     }
   }
 }
