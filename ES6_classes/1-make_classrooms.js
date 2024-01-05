3803ba66efe2b6cd8a8069606d345374f0d5e56a
michaellgans <michaellgans@Victoria>
2024-01-05 10:59:34 -0600

    Fixes Linter (remove .js and parameter ())

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,12 +1,12 @@
 /* Task 1 */
 
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 const initializeRooms = () => {
   /* Stores values in a new array */
   const maxSize = [19, 20, 34];
   /* Creates new instances for each size in array! */
-  return maxSize.map(size => new ClassRoom(size));
+  return maxSize.map((size) => new ClassRoom(size));
 };
 
 export default initializeRooms;
