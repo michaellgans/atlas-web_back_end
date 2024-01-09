3a6126bc058e340ad7515aaf12055e611de66769
michaellgans <michaellgans@Victoria>
2024-01-09 11:37:12 -0600

    Fixes lint Task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,11 +2,11 @@
 
 export default function getListStudentIds(studentList) {
   if (!Array.isArray(studentList)) {
-    const emptylist = []
+    const emptylist = [];
     return emptylist;
   }
-  
-  const newArray = studentList.map(item => item.id);
-  
+
+  const newArray = studentList.map((item) => item.id);
+
   return newArray;
 }
