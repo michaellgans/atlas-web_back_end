8ea1ed219e8fc710354383f3bee6029a94604a55
michaellgans <michaellgans@Victoria>
2024-01-09 14:27:09 -0600

    Creates task 3

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+/* Task 3 */
+
+export default function getStudentIdsSum(studentList) {
+  const studentSum = studentList.map((item) => item.id).reduce((total, studentID) => total + studentID, 0);
+
+  return studentSum;
+}
