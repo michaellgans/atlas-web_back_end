781ac905ca643d60b46eb4663b668901abe8ac0c
michaellgans <michaellgans@Victoria>
2024-01-09 08:57:01 -0600

    Fixes lint

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,10 @@
 /* Task 0 */
 
-export default function getListStudents () {
-  let studentList = Array.of(
+export default function getListStudents() {
+  const studentList = Array.of(
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   );
   return studentList;
 }
