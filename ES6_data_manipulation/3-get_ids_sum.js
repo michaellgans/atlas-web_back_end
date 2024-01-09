a312be13adbe407545caf0bbe3ae4bd2bb19825e
michaellgans <michaellgans@Victoria>
2024-01-09 14:28:41 -0600

    Edits line length for lint

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,8 @@
 /* Task 3 */
 
 export default function getStudentIdsSum(studentList) {
-  const studentSum = studentList.map((item) => item.id).reduce((total, studentID) => total + studentID, 0);
+  const studentSum = studentList.map((item) => item.id)
+    .reduce((total, studentID) => total + studentID, 0);
 
   return studentSum;
 }
