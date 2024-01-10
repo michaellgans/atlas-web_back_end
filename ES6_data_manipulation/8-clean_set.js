52ad9af84a4824cd0b8fd400f7b578f3a2bb6111
michaellgans <michaellgans@Victoria>
2024-01-10 08:27:06 -0600

    Sudo code

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+/* Task 8 */
+
+export default function cleanSet(set, startString) {
+
+  for (let item of set) {
+    if (startString is found in set) {
+      const result = result.join("-");
+    }
+  }
+
+  return result;
+}
