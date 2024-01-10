09ede4d88dbb95d1de03ff2924d6bdd328ac43ac
michaellgans <michaellgans@Victoria>
2024-01-10 12:30:39 -0600

    testing error

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -9,7 +9,7 @@ export default function handleResponseFromAPI(promise) {
       });
       console.log('Got a response from the API');
     } else {
-      reject(new Error(''));
+      reject(new Error());
     }
   });
 }
