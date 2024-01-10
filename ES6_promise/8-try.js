f326e505e5e79ed55064de51789e8f9df9158022
michaellgans <michaellgans@Victoria>
2024-01-10 14:12:33 -0600

    Creates task 8

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,2 +1,8 @@
 /* Task 8 */
 
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  return (numerator / denominator);
+}
