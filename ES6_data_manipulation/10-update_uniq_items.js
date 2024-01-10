ff52ba392c87370df705eab84a07172368823e98
michaellgans <michaellgans@Victoria>
2024-01-10 10:06:58 -0600

    adds error throw

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,11 @@
 /* Task 10 */
 
 export default function updateUniqueItems(oldMap) {
+
+  if(!(oldMap instanceof Map)) {
+    throw Error('Cannot process');
+  }
+
   for (let [item, itemValue] of oldMap) {
     if (itemValue === 1) {
       oldMap.set(item, 100)
