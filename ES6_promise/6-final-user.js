1b4d2ee3ca1f28b72e1fab6f2fe39b49d426f530
michaellgans <michaellgans@Victoria>
2024-01-10 13:36:03 -0600

    Sudo Code

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,2 +1,13 @@
 /* Task 6 */
 
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo.reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([signUpUser(), uploadPhoto()])
+    .then ({
+        status: promiseStatus,
+        value: value,
+    })
+    .catch (new Error())
+}
