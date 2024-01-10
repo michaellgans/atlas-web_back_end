5f6b8c0838f2ad6402c2a83a5ac4b45a08b60c5f
michaellgans <michaellgans@Victoria>
2024-01-10 12:14:00 -0600

    Creates task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,1 +1,15 @@
 /* Task 2 */
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    console.log('Got a response from the API');
+    } else {
+      reject(new Error(''));
+    }
+  });
+}
