d104796c90cc172eaa38609a64a568eb601c39be
michaellgans <michaellgans@Victoria>
2024-01-10 14:09:23 -0600

    lint

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
 /* Task 7 */
 
 export default function loadBalancer(chinaDownload, USDownload) {
-    return Promise.race([chinaDownload, USDownload]);
+  return Promise.race([chinaDownload, USDownload]);
 }
