11965e9b59ba8272e63cc4a5f657cf16c1d1f42f
michaellgans <michaellgans@Victoria>
2024-01-10 08:36:42 -0600

    fixes lint

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,15 @@
 /* Task 8 */
 
 export default function cleanSet(set, startString) {
-  let result = "";
+  let result = '';
 
-  if (startString.length == 0) {
-    return "";
+  if (startString.length === 0) {
+    return;
   }
 
-  for (let item of set) {
+  for (const item of set) {
     if (item.startsWith(startString)) {
-      result += item.substring(startString.length) + "-";
+      result += item.substring(startString.length) + '-';
     }
   }
 
