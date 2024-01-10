316efa4cf765d805618a3184ffb4bdf9e294c854
michaellgans <michaellgans@Victoria>
2024-01-10 09:16:48 -0600

    Creates task 9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+/* Task 9 */
+
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
