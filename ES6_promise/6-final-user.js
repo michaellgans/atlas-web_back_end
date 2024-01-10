f32a4bcc2e742d931378a0b269aa53aadca1d1e3
michaellgans <michaellgans@Victoria>
2024-01-10 14:06:12 -0600

    revert 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,25 +1,13 @@
 /* Task 6 */
 
 import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject.js';
+import uploadPhoto from './5-photo.reject.js';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const resultArray = [];
-
-  await Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ])
-  .then (async (result) => {
-      resultArray.push({
-        status: result.status,
-        value: result.value,
-      });
-  })
-  .catch ((error) => {
-    resultArray.push({
-      status: 'rejected',
-      value: error.toString(),
-    });
-  });
+  return Promise.all([signUpUser(), uploadPhoto()])
+    .then ({
+        status: promiseStatus,
+        value: value,
+    })
+    .catch (new Error())
 }
