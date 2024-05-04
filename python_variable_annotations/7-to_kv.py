2c708f1c66e50892d94f15b316d360f5dc8030ba
michaellgans <michaellgans@Victoria>
2024-05-03 23:21:53 -0500

    M - 11:21 PM - imports Tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Task 7 """
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Union[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Returns a tuple from the variables inputted """
     v_squared: float = v * v
-    return k, v_squared
+    return (k, v_squared)
