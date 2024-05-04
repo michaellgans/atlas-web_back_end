8f0031a4a198e401c8e87c053eec84a805a619f3
michaellgans <michaellgans@Victoria>
2024-05-03 22:23:41 -0500

    M - 10:23 PM - defines variables for the print statements in main

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Task 4 """
+
+
+a: int
+pi: float
+i_understand_annotations: bool
+school: str
+
+a = 1
+pi = 3.14
+i_understand_annotations = True
+school = "Holberton"
