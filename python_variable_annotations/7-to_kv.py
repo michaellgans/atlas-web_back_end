6986641480e735290f555aeb3b3b2ab33611584c
michaellgans <michaellgans@Victoria>
2024-05-03 23:17:44 -0500

    editing typing

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -7,4 +7,4 @@ from typing import Union
 def to_kv(k: str, v: Union[int, float]) -> Union[str, float]:
     """ Returns a tuple from the variables inputted """
     v_squared: float = v * v
-    return (k, v_squared)
+    return k, v_squared
