2447e1f904386528579627d297640d9a31cc7447
michaellgans <michaellgans@Victoria>
2024-05-05 19:24:09 -0500

    M - 7:23 PM - Removes the sort function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,5 +10,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Returns a list of all the delays """
     async_tasks = [wait_random(max_delay) for x in range(n)]
     delay_list = await asyncio.gather(*async_tasks)
-    delay_list.sort()
     return delay_list
