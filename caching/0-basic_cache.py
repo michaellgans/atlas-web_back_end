e7dd83b993e215a2184e220972f2120b77acb8e6
michaellgans <michaellgans@Victoria>
2024-05-06 15:35:35 -0500

    M - 3:35 PM - Changes documentation

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python3
-""" Task 0 """
+""" Task 0
+"""
 
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ Child class of BaseCaching """
+    """ Child class of BaseCaching:
+        - Creates a caching system
+        - Stores values in key[value] pairs
+    """
     def __init__(self):
+        """ Initiliaze
+        """
         super().__init__()
         self.cache = self.cache_data
 
     def put(self, key, item):
-        """ Puts items in the cache """
+        """ Puts items in the cache
+        """
         if key is None or item is None:
             pass
         self.cache[key] = item
 
     def get(self, key):
-        """ Fetches items in the cache """
+        """ Fetches items in the cache
+        """
         if key is None:
             return None
 
