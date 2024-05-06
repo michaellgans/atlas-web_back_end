78b37a371d492fb89fc364bb2c78cd4b76b8f8b1
michaellgans <michaellgans@Victoria>
2024-05-05 19:56:08 -0500

    M - 7:56 PM - fixes pycodestyle for task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -13,4 +13,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     for x in range(n):
         delay_list.append(asyncio.create_task(wait_random(max_delay)))
 
-    return [await delay_item for delay_item in asyncio.as_completed(delay_list)]
+    return [await item for item in asyncio.as_completed(delay_list)]
