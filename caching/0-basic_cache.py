191e9d00f0a6404f9498718c8c21193db6a2e0a2
michaellgans <michaellgans@Victoria>
2024-05-06 15:30:33 -0500

    M - 3:30 PM - Creates the bascaching

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -4,13 +4,26 @@
 from base_caching import BaseCaching
 
 
-class BaseCache(BaseCaching):
+class BasicCache(BaseCaching):
     """ Child class of BaseCaching """
+
     def __init__(self):
         """ Initializes variables """
-        super().__init()
+        super().__init__()
+        self.cache = self.cache_data
 
     def put(self, key, item):
         """ Puts items in the cache """
-        
-        
+        if key is None or item is None:
+            pass
+        self.cache[key] = item
+
+    def get(self, key):
+        """ Fetches items in the cache """
+        if key is None:
+            return None
+
+        if key not in self.cache:
+            return None
+        else:
+            return self.cache.get(key)
