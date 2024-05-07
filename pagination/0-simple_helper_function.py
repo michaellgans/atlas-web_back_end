aa285440f47fbf7c4e8f27dde20e2751502d783e
michaellgans <michaellgans@Victoria>
2024-05-07 14:40:52 -0500

    M - 2:40 PM - Creates a helper function to display the index of items needed to be displayed on a given page

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Task 1 - Simple Helper Function """
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """ Creates a helper function to know what index series to show """
+    start_index: int = (page - 1) * page_size
+    end_index: int = page * page_size
+
+    return (start_index, end_index)
