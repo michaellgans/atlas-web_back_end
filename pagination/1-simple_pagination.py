ce59c3e12dd05c98557cff583ce50ccb949359ee
michaellgans <michaellgans@Victoria>
2024-05-13 10:08:01 -0500

    M - 10:07 AM - Adds documentation to get_page

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -15,16 +15,14 @@ def index_range(page: int, page_size: int) -> Tuple:
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """ Server class to paginate a database of popular baby names. """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """ Cached dataset """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -34,6 +32,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Gets the data for a specific page """
         assert type(page) is int and type(page_size) is int
         assert page > 0 and page_size > 0
 
