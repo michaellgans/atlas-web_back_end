b8f0babbfa46a9a0cf64dc1ce68f286cf58dd380
michaellgans <michaellgans@Victoria>
2024-05-20 15:36:58 -0500

    M - 3:36 PM - Attempts regex pattern

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,12 +8,16 @@ from typing import List
 def filter_datum(
         fields: List[str], redaction: str,
         message: str, separator: str) -> str:
-    """ 
+    """
     Returns data obfuscated based on Regex Filtering
-    
+
     Args
     - fields: what to redact
     - redaction: what to redact the characters with
     - message: the origional string before redaction
     - separator: what to separate the strings with
      """
+    for item in fields:
+        reg_match = fr'({item}=)([^;{separator}]+)'
+        message = re.sub(reg_match, fr'\1{redaction}', message)
+    return message
