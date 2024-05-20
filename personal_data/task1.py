a9fac78fdf3467152cbf4b2c399bbafd6131d828
michaellgans <michaellgans@Victoria>
2024-05-20 16:01:24 -0500

    M - 4:01 PM - Creates CSV and backs up task 1

---
+++ personal_data/task1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Task 0 - Regex-ing """
+
+import re
+import logging
+from typing import List
+
+
+def filter_datum(
+        fields: List[str], redaction: str,
+        message: str, separator: str) -> str:
+    """
+    Returns data obfuscated based on Regex Filtering
+
+    Args
+    - fields: what to redact
+    - redaction: what to redact the characters with
+    - message: the origional string before redaction
+    - separator: what to separate the strings with
+     """
+    for item in fields:
+        reg_match = fr'({item}=)([^;{separator}]+)'
+        message = re.sub(reg_match, fr'\1{redaction}', message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter Class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Initializes Varaibles """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Redacts specific fields """
+        og_mess = super().format(record)
+        redacted_message = filter_datum(
+            self.fields, self.REDACTION, og_mess, self.SEPARATOR)
+        return redacted_message
