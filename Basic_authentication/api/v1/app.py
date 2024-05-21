5541725ad33379dc83bb684b32a2a29055dfe86e
michaellgans <michaellgans@Victoria>
2024-05-21 12:35:14 -0500

    Adds error handler to app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -13,6 +13,11 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Task 1 - Unauthorized User
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 @app.errorhandler(404)
 def not_found(error) -> str:
