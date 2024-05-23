46a5f04fd516e9fc9ca96580f328d9b7dd52aa15
michaellgans <michaellgans@Victoria>
2024-05-23 09:59:20 -0500

    M - 9:59 AM - Fixes pycodestyle

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -13,6 +13,7 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def abort401() -> str:
     """ GET /api/v1/unauthorized
@@ -20,6 +21,7 @@ def abort401() -> str:
       - the status of the API
     """
     return abort(401)
+
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
