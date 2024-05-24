f4eb279e3a2b8f8f3fdacc80ea30ab4ffc4630c7
michaellgans <michaellgans@Victoria>
2024-05-23 20:29:20 -0500

    M - 8:29 PM - Decodes base64

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Task 6 - Basic Authorization """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,20 @@ class BasicAuth(Auth):
 
         header_snip = authorization_header[len("Basic "):]
         return header_snip
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Task 8 - Base64 Decode """
+        base_header = base64_authorization_header
+
+        if base_header is None:
+            return None
+        if not isinstance(base_header, str):
+            return None
+
+        try:
+            decoded = base64.b64decode(base_header)
+            decoded_utf8 = decoded.decode("utf-8")
+            return decoded_utf8
+        except binascii.Error:
+            return None
