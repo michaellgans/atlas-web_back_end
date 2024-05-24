cd0e0b1a11126ed786a256cb39647bd701d78074
michaellgans <michaellgans@Victoria>
2024-05-23 20:18:36 -0500

    M - 8:18 PM - Adds Base64 Method

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,16 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Task 6 - Basic Authorization """
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ Task 7 - Base64 """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        header_snip = authorization_header[len("Basic "):]
+        return header_snip
