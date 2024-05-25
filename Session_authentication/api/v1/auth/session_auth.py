771c704bd8f5785b56ae06e01941d1712fe2ccd4
michaellgans <michaellgans@Victoria>
2024-05-24 20:22:04 -0500

    M - 8:21 PM - Creates Session ID

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,21 @@
 """ Task 1 - Session Auth """
 
 from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
-    """ Task 1 - Empty """
-    pass
+    """ Task 2 - Create a Session """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a Session ID for user_id """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
