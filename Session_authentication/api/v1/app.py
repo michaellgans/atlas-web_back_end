ec3b42e51a7c215d3932cb8fd392f7272160f330
michaellgans <michaellgans@Victoria>
2024-05-24 20:50:32 -0500

    M - 8:50 PM - Edits if logic for before_request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -42,17 +42,14 @@ def before_request() -> str:
     if not auth.require_auth(request.path, path_list):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None\
+            and auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
         abort(403)
 
     request.current_user = auth.current_user(request)
-
-    if auth.authorization_header(request) is None\
-            and auth.session_cookie(request) is None:
-        abort(401)
 
 
 @app.errorhandler(403)
