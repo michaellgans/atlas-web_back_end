ae20857bfc01fbbb94e76c8057ef74ba152609e7
michaellgans <michaellgans@Victoria>
2024-05-25 12:55:27 -0500

    M - 12:55 PM - turns echo off to simply output

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -11,21 +11,18 @@ from user import User
 
 
 class DB:
-    """DB class
-    """
+    """ DB class """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """ Initialize a new DB instance """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """ Memoized session object """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
