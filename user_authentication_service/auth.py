4e8728c307901e2a170a35414db273d7638a03c4
michaellgans <michaellgans@Victoria>
2024-05-25 21:31:19 -0500

    M - 9:31 PM - moves hashed password outside of class

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -7,15 +7,15 @@ from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(password: str) -> bytes:
+    """ Task 4 - Salted Password """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
 class Auth:
     """ Task 5 - requires email and password """
     def __init__(self):
         """ Private instance of DB """
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """ Task 4 - Salted Password """
-        return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
     def register_user(self, email: str, password: str) -> User:
         """ Task 5 - adds email and password to user data """
@@ -23,5 +23,5 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
