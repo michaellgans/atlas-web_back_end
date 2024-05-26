f62842fb90a3a4dcef0a8e4ce777cdad39a317d2
michaellgans <michaellgans@Victoria>
2024-05-26 11:12:41 -0500

    M - 11:12 AM - gets user from session id

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -51,3 +51,13 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """ Task 11 - Gets the user from Session ID """
+        if session_id is None:
+            return None
+        try:
+            user_info = self._db.find_user_by(session_id=session_id)
+            return user_info
+        except NoResultFound:
+            return None
