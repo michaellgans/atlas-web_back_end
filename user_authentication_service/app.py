07a164d49155a929dcf86628f6510f18a092f8bd
michaellgans <michaellgans@Victoria>
2024-05-26 12:14:27 -0500

    M - 12:12 PM - Updates Password

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -83,5 +83,22 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
+@app.route("/update_password", methods=["PUT"], strict_slashes=False)
+def update_password():
+    """ Task 19 - Updates Password """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+
+    if not email or not reset_token or not new_password:
+        abort(400)
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
