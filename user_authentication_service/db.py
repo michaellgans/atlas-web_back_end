38a6c0eac5510c41eb472b1fba3c66cf7226d0f7
michaellgans <michaellgans@Victoria>
2024-05-25 20:41:48 -0500

    M - 8:41 PM - Adds update user method

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -50,3 +50,16 @@ class DB:
             raise NoResultFound
 
         return User
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ Task 3 - Updates User """
+        user = self.find_user_by(id=user_id)
+
+        for key, value in kwargs.items():
+            if key not in user.__dict__:
+                raise ValueError
+
+            setattr(user, key, value)
+            self._session.commit()
+
+            return None
