47c90dc647c9cba0ee30bcc8b03f147a7d9163c4
michaellgans <michaellgans@Victoria>
2024-05-25 21:14:30 -0500

    M - 9:14 PM - Register User method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,8 +2,26 @@
 """ Task 4 - Hash Password """
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    """ Task 4 - Salted Password """
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+class Auth:
+    """ Task 5 - requires email and password """
+    def __init__(self):
+        """ Private instance of DB """
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """ Task 4 - Salted Password """
+        return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Task 5 - adds email and password to user data """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            return self._db.add_user(email, hashed_password)
