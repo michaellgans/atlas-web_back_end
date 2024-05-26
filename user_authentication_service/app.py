39b664049954bfb3848c62adee900b96247852db
michaellgans <michaellgans@Victoria>
2024-05-26 11:56:44 -0500

    M - 11:56 AM - Gets reset token

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -69,5 +69,19 @@ def profile():
         abort(403)
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token():
+    """ Task 17 - Get Password Reset Token """
+    email = request.form.get("email")
+
+    try:
+        user = AUTH._db.find_user_by(email=email)
+    except NoResultFound:
+        abort(403)
+
+    reset_token = AUTH.get_reset_password_token(email)
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
