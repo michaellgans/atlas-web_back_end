7458acd7afebe247d04330c5aad61f9b39d6041c
michaellgans <michaellgans@Victoria>
2024-05-26 10:32:13 -0500

    M - 10:32 AM - Changes return of UUID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -15,7 +15,7 @@ def _hash_password(password: str) -> bytes:
 
 def _generate_uuid() -> str:
     """ Task 9 - Generate a UUID """
-    return str(uuid.uuid4)
+    return str(uuid.uuid4())
 
 
 class Auth:
