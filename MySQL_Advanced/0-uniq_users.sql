cb761bf1d453b27bedad2cad82b24417ad3d933d
michaellgans <michaellgans@Victoria>
2024-06-11 15:10:05 -0500

    M - 3:09 PM - Solves for users not existing

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- Task 0: Creates Users Table
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
