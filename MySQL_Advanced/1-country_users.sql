fa794bdb1f3a6712f7a566ec0315247e7f2f30a6
michaellgans <michaellgans@Victoria>
2024-06-11 15:19:36 -0500

    M - 3:19 PM - Adds country column

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Task 1: Creates Users Table
+CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country NOT NULL ENUM("US", "CO", "TN") DEFAULT "US"
+);
