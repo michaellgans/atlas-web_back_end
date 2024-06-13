64dfe2df1fd62faa9a343ec4343856c7dc75e3fb
michaellgans <michaellgans@Victoria>
2024-06-12 23:25:31 -0500

    M - 11:25 PM - Task 9

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,2 +1,17 @@
-// This is a comment
-THIS is your code;
+#!/usr/bin/env python3
+""" Task 9 - Insert a Document in Python """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document into a collection
+
+    Arguments:
+     - mong_collection: the collection
+
+    Returns:
+     - The inserted id (key: value)
+    """
+    results_cursor = mongo_collection.insert_one(kwargs)
+
+    return results_cursor.inserted_id
