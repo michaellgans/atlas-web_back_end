8c69c7b5aea66709cac09f3991bfac2cbb5bf8c1
michaellgans <michaellgans@Victoria>
2024-06-12 23:34:58 -0500

    M - 11:34 PM - Task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,2 +1,23 @@
-// This is a comment
-THIS is your code;
+#!/usr/bin/env python3
+""" Task 11 - Where can I learn Python? """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    List all documents in mongo_collection
+
+    Arguments:
+     - mong_collection: the collection
+     - topic: subject taught at school
+
+    Returns:
+     - All documents in that collection
+    """
+    results_cursor = mongo_collection.find({"topics": topic})
+
+    documents_found = []
+
+    for document in results_cursor:
+        documents_found.append(document)
+
+    return documents_found
--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,2 +1,22 @@
-// This is a comment
-THIS is your code;
+#!/usr/bin/env python3
+""" Task 8 - Lists all documents in a collection """
+
+
+def list_all(mongo_collection):
+    """
+    List all documents in mongo_collection
+
+    Arguments:
+     - mong_collection: the collection
+
+    Returns:
+     - All documents in that collection
+    """
+    results_cursor = mongo_collection.find({})
+
+    documents_found = []
+
+    for document in results_cursor:
+        documents_found.append(document)
+
+    return documents_found
