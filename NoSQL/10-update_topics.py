c99f1349c6c77504c2f7b8083ce6b0706f9165f5
michaellgans <michaellgans@Victoria>
2024-06-12 23:31:15 -0500

    M - 11:31 PM - Task 10

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,2 +1,20 @@
-// This is a comment
-THIS is your code;
+#!/usr/bin/env python3
+""" Task 10 - Change school topics """
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a document by name.
+
+    Arguments:
+     - mong_collection: the collection
+     - name: name of school
+     - topic: subject at school
+
+    Returns:
+     - Key: Value pairs
+    """
+    results_cursor = mongo_collection.update_many({"name": name},\
+                     {"$set": {"topics": topics}})
+
+    return results_cursor.modified_count
