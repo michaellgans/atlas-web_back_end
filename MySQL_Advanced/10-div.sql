fa00c972a519a2b825cbc5e53cd8b12ee047b084
michaellgans <michaellgans@Victoria>
2024-06-12 19:47:17 -0500

    M - 7:47 PM - Does division

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,19 @@
+-- Task 10: Divides two numbers
+DROP FUNCTION IF EXISTS SafeDiv;
+
+DELIMITER $$
+CREATE FUNCITON SafeDiv(a FLOAT, b FLOAT)
+RETURNS FLOAT
+BEGIN
+    DECLARE result FLOAT;
+
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+
+    RETURN result;
+END $$
+
+DELIMITER ;
