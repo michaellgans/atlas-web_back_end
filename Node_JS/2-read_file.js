19cf53e6585189857cc53da723d9322fdad1966d
michaellgans <michaellgans@Victoria>
2024-06-25 15:50:59 -0500

    M - 3:50 PM - Saves data from CSV into an object and prints reports based on that

--- Node_JS/2-read_file.js
+++ Node_JS/2-read_file.js
@@ -4,25 +4,38 @@
 const fs = require('fs');
 
 function countStudents(path) {
-    // Reads file from path synchronously
-    fs.readFile(path, 'utf8', (err, data) => {
-        if (err) {
-            // If can't find database, throw error
-            console.log('Cannot load the database');
-            return;
-        }
-        console.log(`I found this: \n${data}`);
+  // Reads file from path synchronously
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      // If can't find database, throw error
+      console.log('Cannot load the database');
+      return;
+    }
+
+    // Finding number of students
+    const lines = data.trim().split('\n').slice(1);
+    const records = lines.length;
+
+    console.log(`Number of students: ${records}`);
+
+    // Pulling first name and field into an object
+    const nameByField = {};
+
+    lines.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+
+      // Sorts first names into arrays by field
+      if (!nameByField[field]) {
+        nameByField[field] = [];
+      }
+      nameByField[field].push(firstname);
     });
+
+    // Prints number of students by field and lists the students
+    Object.entries(nameByField).forEach(([field, listOfStudents]) => {
+      console.log(`Number of students in ${field}: ${listOfStudents.length}. List: ${listOfStudents.join(', ')}`);
+    });
+  });
 }
 
-//     // If database found, display Number of Students
-//     const num_of_students = 
-//     const field = 
-//     const students_in_field = 
-//     const list_in_field = 
-
-//     console.log(`Number of students: ${num_of_students}`)
-//     console.log(`Number of students in ${field}: ${students_in_field}`);
-//   }
-  
-  module.exports = countStudents;
+module.exports = countStudents;
