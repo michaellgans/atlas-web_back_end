1e9bc9327ccc7b838b8c602358558cd8434a260b
michaellgans <michaellgans@Victoria>
2024-06-25 12:53:40 -0500

    M - 12:53 PM - Creates mains and base file

--- Node_JS/1-stdin.js
+++ Node_JS/1-stdin.js
@@ -1,20 +1,9 @@
 // Task 1 - Using Process stdin
 
-// Prints a prompt and answer goes on the next line
-console.log('Welcome to Atlas School, what is your name?');
-
-// Listen for standard input
-process.stdin.on('data', (input) => {
-  console.log(`\nYour name is: ${input}`);
-
-  // Makes exit process wait for better UX
-  setTimeout(() => {
-    process.exit();
-  }, 1000);
-});
-
-// Listen for "exit"
-process.on('exit', () => {
-  console.log('This important software is now closing...');
-  console.log('Bye Tomas!');
-});
+function displayMessage(message) {
+    // Prints a message to STDOUT
+  
+    console.log(message);
+  }
+  
+  module.exports = displayMessage;
---
+++ Node_JS/2-read_file.js
@@ -0,0 +1,19 @@
+// Task 2 - Read csv file
+
+function countStudents(path) {
+    // Reads file from path synchronously
+
+    // If can't find database, throw error
+    console.log('Connot load the database')
+    
+    // If database found, display Number of Students
+    const num_of_students = 
+    const field = 
+    const students_in_field = 
+    const list_in_field = 
+
+    console.log(`Number of students: ${num_of_students}`)
+    console.log(`Number of students in ${field}: ${students_in_field}`);
+  }
+  
+  module.exports = displayMessage;
