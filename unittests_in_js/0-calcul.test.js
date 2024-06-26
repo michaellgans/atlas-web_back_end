097ae6ef8b2226d11a1b2e4f889b7cae17eef47e
michaellgans <michaellgans@Victoria>
2024-06-26 17:00:48 -0500

    M - 5:00 PM - Test passing locally

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,3 +1,10 @@
 // Task 0 - Tests 0-calcul.js
 
+const assert = require('assert');
+const calculateNumbers = require('./0-calcul');
 
+describe('calculateNumbers', function() {
+  it('should take in two numbers, round them, and return the sum', function() {
+    assert.strictEqual(2.2, 2.2), 4;
+  });
+});
