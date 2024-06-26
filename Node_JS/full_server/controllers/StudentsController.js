3096ff5a56eb9cbc610ef98d1c67e9144b07e7f4
michaellgans <michaellgans@Victoria>
2024-06-26 16:01:15 -0500

    M - 4:00 PM - Output is printed, need to tweak what it says and check ESlint

--- Node_JS/full_server/controllers/StudentsController.js
+++ Node_JS/full_server/controllers/StudentsController.js
@@ -4,11 +4,12 @@ import readDatabase from '../utils';
 
 class StudentsController {
   // Controls information retrieved from database
-  static getAllStudents(req, res) {
+  static async getAllStudents(req, res) {
     try {
       // Grabs the CSV file from user input
-      const readDatabase = process.argv[2];
-  
+      const databasePath = process.argv[2];
+      const result = await readDatabase(databasePath);
+
       // Sends Task 3 output
       res.type('type/plain');
       res.status(200).send(`This is the list of students\n${result}`);
@@ -19,14 +20,27 @@ class StudentsController {
     }
   }
 
-  static getAllStudentsByMajor(req, res) {
+  static async getAllStudentsByMajor(req, res) {
     try {
       // Grabs the CSV file from user input
-      const readDatabase = process.argv[2];
-  
-      // Sends Task 3 output
+      const databasePath = process.argv[2];
+      const result = await readDatabase(databasePath);
+
+      // If major passed to route is not CS or SWE
+      const major = req.params.major;
+      if (!['CS', 'SWE'].includes(major)) {
+        res.status(400).send('Major parameter must be CS or SWE');
+        return;
+      }
+
+      // Grabs students by major passed to the route
+      const students = result
+        .split('\n')
+        .filter(line => line.includes(`in ${major}`))
+        .join('\n');
+      
       res.type('type/plain');
-      res.status(200).send(`This is the list of students\n${result}`);
+      res.status(200).send(`${students}`);
     } catch (error) {
       // Catches Status 500
       console.log(error);
