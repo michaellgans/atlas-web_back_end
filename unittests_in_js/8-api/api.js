78dda253f31473b72d87476fad8b447b22bcc73d
michaellgans <michaellgans@Victoria>
2024-06-26 21:21:00 -0500

    M - 9:20 PM - Installs new package.json

---
+++ unittests_in_js/8-api/api.js
@@ -0,0 +1,10 @@
+// Task 8 - Basic Integration Testing
+
+const express = require('express');
+const FUNCITON = require('FILE');
+
+describe('FUNCITON', function() {
+  it('DESCRIPTION OF TEST', function() {
+    assert.strictEqual(FUNCTION(A, B), EXPECTED);
+  });
+});
---
+++ unittests_in_js/8-api/api.test.js
@@ -0,0 +1,10 @@
+// Task # - Tests FILE
+
+const assert = require('assert');
+const FUNCITON = require('FILE');
+
+describe('FUNCITON', function() {
+  it('DESCRIPTION OF TEST', function() {
+    assert.strictEqual(FUNCTION(A, B), EXPECTED);
+  });
+});
