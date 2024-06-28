7116270db16cf52e5972426a249f0caed98cbd1c
michaellgans <michaellgans@Victoria>
2024-06-27 20:55:33 -0500

    M - 8:55 PM - Tests if jobs is an array

--- queuing_system_in_js/8-job.test.js
+++ queuing_system_in_js/8-job.test.js
@@ -4,7 +4,7 @@ const { expect } = require('chai');
 const createPushNotificationsJobs = require('./8-job');
 
 describe('createPushNotificationsJobs', function() {
-  it('should validate which jobs are inside the queue', function(done) {
+  it('should test that jobs is an array', function(done) {
     expect(() => createPushNotificationsJobs({}, queue).to.throw(Error, 'Jobs is the wrong type'));
   });
 });
