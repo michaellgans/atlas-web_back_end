81f9e5f5d88a30c5c3c8aaab65ca4ab1b5efab39
michaellgans <michaellgans@Victoria>
2024-06-27 19:42:27 -0500

    M - 7:42 PM - Job creator

---
+++ queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,24 @@
+// Task 6 - Create the Job Creator
+
+const kue = require('kue');
+
+// The Queue
+const push_notification_code = kue.createQueue();
+
+// The Job
+const jobObject = {
+    phoneNumber: 867-5309,
+    message: 'Jenny\'s Number',
+};
+
+const job = push_notification_code.create('push_notification', jobObject).save((err) => {
+    if (!err) console.log('Notification job created:', job.id);
+});
+
+job.on('complete', () => {
+    console.log('Notification job completed');
+});
+
+job.on('failed', (err) => {
+    console.log('Notification job failed');
+});
